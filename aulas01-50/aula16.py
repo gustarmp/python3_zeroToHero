ead489f2ad74b00e917fdf0bb0bb854603554bbe
gustarmp <gustavo360@protonmail.com>
2025-04-03 02:34:35 -0300

    adicao de aula17-23

--- aulas01-50/aula16.py
+++ aulas01-50/aula16.py
@@ -1,11 +1,11 @@
 # if / elif      / else
 # se / e se      / se nao
 
-entrada = input('Voce quer "entrar" ou "sair?" ')
+entrada = input('Voce quer "entrar" ou "sair?" ').lower() # lower usado para formatar a string para tudo minusculo.
 
-if entrada == 'entrar' or entrada == 'Entrar':
+if entrada == 'entrar':
     print('Voce entrou no sistema.')
-elif entrada == 'sair' or entrada == 'Sair':
+elif entrada == 'sair':
     print('Voce saiu do sistema.')
 else:
     print('Voce nao digitou uma opcao valida.')
---
+++ aulas01-50/aula20.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+
+if num1 > num2:
+    print('O primeiro valor e maior!')
+else:
+    print('O segundo valor e maior!')
